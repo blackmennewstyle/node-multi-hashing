c89728c4a99063c6b703c51b01a51fbf4ae7a7de
ceedii <ceedii@europa.cedric-crispin.com>
2022-10-21 15:11:22 +0000

    Fixing dependencies issue

--- src/verthash.h
+++ src/verthash.h
@@ -1,8 +1,11 @@
-#ifndef VERTHASH_H
-#define VERTHASH_H
+#ifndef VERTHASH_H_INCLUDED
+#define VERTHASH_H_INCLUDED
 
+#include <stddef.h>
+#include <stdint.h>
 #include <stdint.h>
 
 void verthash_hash(const unsigned char* blob_bytes, const size_t blob_size, const unsigned char* input, const size_t input_size, unsigned char* output);
+int get_verthash_file();
 
 #endif
